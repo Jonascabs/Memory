3dac95a4fc4a2cd689fc5c2b922a16afec5873df
Jonascabs <jonassaint.cabili@g.msuiit.edu.ph>
2018-12-05 20:39:48 +0800

    Update Pattern.cpp

--- Project/Source Code/Pattern/Pattern.cpp
+++ Project/Source Code/Pattern/Pattern.cpp
@@ -5,10 +5,10 @@
 
 #include <Pattern.h> //include the declaration for this class
 #include <Arduino.h>
-const int Chip_Enable1 = 41
-const int Chip_Enable2 = 43
-const int Write_enable = 45
-const int Read_enable  = 47
+const int Chip_Enable1 = 41;
+const int Chip_Enable2 = 43;
+const int Write_enable = 45;
+const int Read_enable  = 47;
 
 
 //<<constructor>>
