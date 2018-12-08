9da340d392e286257566ef924d681d54e1d40fc8
Jonascabs <jonassaint.cabili@g.msuiit.edu.ph>
2018-12-08 17:06:19 +0800

    Retrieve Files

---
+++ Project/Source Code/Pattern/Pattern.h
@@ -0,0 +1,28 @@
+#ifndef Pattern_H
+#define TPattern_H
+
+
+#include <Arduino.h>
+
+class Pattern
+{ 
+	
+	public:
+		Pattern();
+		
+		void write_control();
+		
+		void read_control();
+		
+		void write_zero();
+		
+		void write_one();
+		
+		void diagonal(uint8_t index_number);
+		
+		void read();
+		
+		void checkerboard();
+};
+
+#endif
